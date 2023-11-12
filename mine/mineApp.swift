8d66e77c6018cd36c820827d40684f5bb775af52
Alkaid <zhonghaoxiang126@126.com>
2023-11-13 00:13:03 +0800

    Initial Commit

---
+++ mine/mineApp.swift
@@ -0,0 +1,17 @@
+//
+//  mineApp.swift
+//  mine
+//
+//  Created by Haoxiang Zhong on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct mineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
